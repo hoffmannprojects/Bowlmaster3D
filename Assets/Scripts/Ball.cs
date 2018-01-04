4bd709b91ea1338c64a437262d964c127ec5dd34
Tim <tim@hoffmannprojects.com>
2018-01-04 18:46:38 +0100

    ball and pins set up. before changing physics. lecture 192.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float launchSpeed;
+    public Vector3 velocity;
+
+    private Rigidbody rb;
+    private AudioSource audioSource;
+    private Vector3 force;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+    }
+
+    public void Launch()
+    {
+        force.x = velocity.x * launchSpeed;
+        force.y = velocity.y * launchSpeed;
+        force.z = velocity.z * launchSpeed;
+
+        rb.AddForce(force, ForceMode.Impulse);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
