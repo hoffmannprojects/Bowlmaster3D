c63d7d23a604755891128b189d63ec72c25fd3db
Tim Hoffmann <tim@hoffmannprojects.com>
2018-01-25 13:52:15 +0100

    Lecture 202: Ball gets detected when entering Triggerbox; Pins get destroyed when leaving Triggerbox.

--- Assets/Ball & Pins/PinSetter.cs
+++ Assets/Ball & Pins/PinSetter.cs
@@ -5,16 +5,45 @@ using UnityEngine.UI;
 
 public class PinSetter : MonoBehaviour
 {
+    private bool ballEnteredBox = false;
+    private Text pinCounterDisplay;
+
     // Use this for initialization
     void Start()
     {
+        pinCounterDisplay = GameObject.Find("PinCounter").GetComponent<Text>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
         CountStanding();
     }
+
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.gameObject.GetComponent<Ball>())
+        {
+            ballEnteredBox = true;
+            print("Ball detected in PinSetter");
+
+            pinCounterDisplay.color = Color.red;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider otherCollider)
+    {
+        // Check for presence of Pin script on parent, because of Pin setup in hierarchy. 
+        if (otherCollider.GetComponentInParent<Pin>() != null)
+        {
+            print("Pin left Pinsetter");
+            Destroy(otherCollider.GetComponentInParent<Pin>().gameObject);
+        }
+    }
+
 
     int CountStanding()
     {
@@ -30,8 +59,7 @@ public class PinSetter : MonoBehaviour
 
         }
         // Update pin count UI text.
-        Text pinCounterText = GameObject.Find("PinCounter").GetComponent<Text>();
-        pinCounterText.text = standingPinCount.ToString();
+        pinCounterDisplay.text = standingPinCount.ToString();
 
         // return count of standing pins
         return standingPinCount;
--- Assets/Input Events/SwipeLaunch.cs
+++ Assets/Input Events/SwipeLaunch.cs
@@ -2,48 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof (Ball))] // make sure there is a Ball componet present on the object.
+[RequireComponent(typeof(Ball))] // make sure there is a Ball componet present on the object.
 
-public class SwipeLaunch : MonoBehaviour {
+public class SwipeLaunch : MonoBehaviour
+{
     private Ball ball;
-
     private float swipeStartTime, swipeEndTime;
-
     private Vector3 swipeStartPosition, swipeEndPosition;
 
+    public float speedMultiplier = 1;
+
     // Use this for initialization
-    void Start () {
-        ball = GetComponent<Ball>();       
+    void Start()
+    {
+        ball = GetComponent<Ball>();
     }
 
-    public void SwipeStart() {
+    public void SwipeStart()
+    {
         // capture time and position of drag start.
         swipeStartTime = Time.time;
         swipeStartPosition = Input.mousePosition;
     }
 
-    public void SwipeEnd() {
+    public void SwipeEnd()
+    {
         // Capture duration and position.
         swipeEndTime = Time.time;
         swipeEndPosition = Input.mousePosition;
 
-        float dragDuration = swipeEndTime - swipeStartTime;
+        float dragDuration = (swipeEndTime - swipeStartTime) / speedMultiplier;
 
         // translate x and y drag components into world space
         float launchSpeedX = (swipeEndPosition.x - swipeStartPosition.x) / dragDuration; // Speed = distance / time
         float launchSpeedZ = (swipeEndPosition.y - swipeStartPosition.y) / dragDuration;
 
         // Modify force vector to translate to game dimensions
-        Vector3 launchVelocity = new Vector3 (launchSpeedX, 0, launchSpeedZ);
-  
-        // Launch ball.
-        Debug.Log("drag duration at launch = " + dragDuration);
-        Debug.Log("Velocity at launch = " + launchVelocity);
-        ball.Launch (launchVelocity);
-}
+        Vector3 launchVelocity = new Vector3(launchSpeedX, 0, launchSpeedZ);
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+        // Launch ball.
+        // Debug.Log("drag duration at launch = " + dragDuration);
+        // Debug.Log("Velocity at launch = " + launchVelocity);
+        ball.Launch(launchVelocity);
+    }
+
 }
