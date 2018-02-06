bb51bfeca3cc33bc42e4fba916f6add8a1068cb3
Tim <tim@hoffmannprojects.com>
2018-02-06 12:06:09 +0100

    Set up TDD with Unity Test Runner

---
+++ Assets/ActionMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster
+{
+    public enum Action
+    {
+        Tidy, EndTurn
+    }
+
+    public Action Bowl (int pins)
+    {
+        if ( pins == 10)
+        {
+            return Action.EndTurn;
+        }
+        return Action.Tidy;
+    }
+
+}
