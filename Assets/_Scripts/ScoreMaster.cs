814c74c43985b6e9e85540cc8cfe238d03dedc7a
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-03-14 18:01:56 +0100

    L225: ScoreMaster static class backbone + imported ScoreMasterTest

---
+++ Assets/_Scripts/ScoreMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster {
+
+    public static List<int> ScoreFrames (List<int> bowls)
+    {
+        List<int> frameList = new List<int>();
+
+        // code here
+
+        return frameList;
+    }
+}
