e67444919177058ab4657df4e5949e0723e84041
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-03-18 14:30:13 +0100

    L232: An Array Of UI Text

---
+++ Assets/_Scripts/ScoreDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text[] rollTexts = new Text[21];
+    public Text[] frameTexts = new Text[10];
+
+	// Use this for initialization
+	void Start () {
+		foreach (Text text in rollTexts)
+        {
+            text.text = "";
+        }
+
+        foreach (Text text in frameTexts)
+        {
+            text.text = "";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
